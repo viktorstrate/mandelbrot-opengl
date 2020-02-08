1839a8eb1d03db55101802bed9d2b778c3df30f3
viktorstrate <viktorstrate@gmail.com>
2020-02-08 16:33:13 +0100

    Add color to fractal using HSV

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -6,6 +6,8 @@ out vec4 FragColor;
 uniform vec2 uOffset;
 uniform float uZoom;
 
+vec3 hsv2rgb(vec3 c);
+
 void main()
 {
   vec2 startPos;
@@ -18,7 +20,7 @@ void main()
 
   vec2 pos;
   int iteration = 0;
-  int max_iterations = int(100. / (uZoom/2.+1.));
+  int max_iterations = int(1000.);
   max_iterations = min(max_iterations, 10000);
   // int max_iterations = 100;
 
@@ -33,7 +35,20 @@ void main()
   float f_itr = float(iteration);
   float f_itr_max = float(max_iterations);
 
-  float color = f_itr / f_itr_max;
+  float hue = f_itr/f_itr_max;
+  vec3 color = hsv2rgb(vec3(hue, 0.8, 0.9));
 
-  FragColor = vec4(color, color, color, 1.0);
+  if (iteration == max_iterations) {
+    color = vec3(0.);
+  }
+
+
+  FragColor = vec4(color.rgb, 1.0);
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
